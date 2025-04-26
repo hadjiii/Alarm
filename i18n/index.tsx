f98a8ea7fe5a7e6e6535d867e1e85eeecf0c8456
hadjiii <36597604+hadjiii@users.noreply.github.com>
2025-04-26 16:19:47 +0200

    fix(i18n): fix locales import path

--- i18n/index.tsx
+++ i18n/index.tsx
@@ -1,8 +1,8 @@
 import { getLocales } from 'expo-localization';
 import { I18n } from 'i18n-js';
 
-import fr from '@/i18n/locales/fr.json';
-import en from '@/i18n/locales/en.json';
+import fr from './locales/fr.json';
+import en from './locales/en.json';
 
 const i18n = new I18n({ fr, en });
 i18n.locale = getLocales()[0].languageCode ?? 'fr';
