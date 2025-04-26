a2bdc6da2aac3cb683bc16e3aa09f88f4c9cf33c
hadjiii <36597604+hadjiii@users.noreply.github.com>
2025-04-26 18:00:28 +0200

    refactor(town): remove unnecessary read of store timezones

--- app/(tabs)/(clocks)/town.tsx
+++ app/(tabs)/(clocks)/town.tsx
@@ -34,8 +34,6 @@ export default function TownScreen() {
 		setError(null);
 		setIsLoading(true);
 
-		const timezones = useStore.getState().timezones;
-
 		if (timezones) {
 			setIsLoading(false);
 			return;
